01e4ca633f7cbfbfa99d52fc245202637c1e9a53
Aron Fyodor M. Asor <aronasorman@gmail.com>
2012-05-09 22:06:14 +0800

    added relation between section and student  On branch master 	modified:   app/models/section.rb 	modified:   app/models/student.rb 	modified:   db/schema.rb

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,6 @@ class Section < ActiveRecord::Base
   attr_accessible :name, :year
 
   belongs_to :user
+
+  has_many :students
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508090946) do
+ActiveRecord::Schema.define(:version => 20120509081657) do
 
   create_table "followup_interviews", :force => true do |t|
     t.string   "comments"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20120508090946) do
     t.text     "interests"
     t.datetime "created_at",                   :null => false
     t.datetime "updated_at",                   :null => false
+    t.integer  "student_id"
   end
 
   create_table "users", :force => true do |t|
@@ -94,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20120508090946) do
     t.string   "last_sign_in_ip"
     t.string   "password_salt"
     t.string   "name"
+    t.integer  "user_id"
     t.boolean  "is_head_counselor"
     t.boolean  "is_counselor"
     t.boolean  "is_secretary"

99c6bd78c92522dfc7d77d0c5706800078efcad5
Aron Fyodor M. Asor <aronasorman@gmail.com>
2012-05-13 02:37:21 +0800

    changed nature of problem seed to interview type  On branch master 	modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-nature_of_problems = NatureOfProblemType.create([{name: 'Followup Interview'}, {name: 'Routine Interview'}, {name: 'Other'}])
+InterviewType = InterviewType.create([{name: 'Followup Interview'}, {name: 'Routine Interview'}, {name: 'Other'}])
 
 sections = []
 

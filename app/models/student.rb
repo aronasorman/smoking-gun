0a3728ed19dbbfcf8829125a99f59122e8173b89
Aron Fyodor M. Asor <aronasorman@gmail.com>
2012-05-29 12:51:54 +0800

    sections are now created once the registrar data is uploaded

--- app/models/student.rb
+++ app/models/student.rb
@@ -36,7 +36,7 @@ class Student < ActiveRecord::Base
   		student = Student.find_or_create_by_student_id(id)
 
       section = row[:clno][0..1]
-      student.section = Section.find_by_name(section) 
+      student.section = Section.find_or_create_by_name(section) 
       student.first_name = row[:fname]
       student.middle_name = row[:mname]
       student.last_name = row[:lname]
